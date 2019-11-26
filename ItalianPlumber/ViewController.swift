4e8bee77be8d14a1c76dd3157bb61c5e7b1f2f87
Balazs Szamody <fr3qfly@gmail.com>
2019-11-27 10:44:30 +1100

    The clock is ticking

--- ItalianPlumber/ViewController.swift
+++ ItalianPlumber/ViewController.swift
@@ -7,14 +7,52 @@
 //
 
 import UIKit
+import Combine
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+protocol ViewModel {
+    var timeString: PassthroughSubject<String?, Never> { get }
+    var disposeBag: DisposeBag { get set }
 }
 
+class ViewModelImp: ViewModel {
+    let timeString: PassthroughSubject<String?, Never> = PassthroughSubject()
+    var timer: Timer!
+    var disposeBag = DisposeBag()
+    
+    @objc func fireTimer(_ sender: Timer) {
+        let date = sender.fireDate
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss"
+        formatter.timeZone = TimeZone.current
+        timeString.send(formatter.string(from: date))
+    }
+    
+    init() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(fireTimer(_:)), userInfo: nil, repeats: true)
+    }
+}
+
+class ViewController: UIViewController {
+    var viewModel: ViewModel!
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewModel = ViewModelImp()
+        
+        viewModel
+            .timeString
+            .sink(receiveValue: {[weak self] (time) in
+                self?.label.text = time
+            })
+            .disposed(by: &viewModel.disposeBag)
+    }
+}
+
+typealias DisposeBag = [AnyCancellable]
+
+extension AnyCancellable {
+    func disposed(by disposeBag: inout DisposeBag) {
+        disposeBag.append(self)
+    }
+}
